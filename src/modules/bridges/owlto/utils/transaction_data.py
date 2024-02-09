da761a03054426f205aa8f0881f6c41548c77815
nftscripts <govno123@google.ru>
2024-02-09 08:04:40 +0300

    update owlto address

--- src/modules/bridges/owlto/utils/transaction_data.py
+++ src/modules/bridges/owlto/utils/transaction_data.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-from web3.contract import Contract
 from web3.types import TxParams
 from eth_typing import Address
 from web3 import Web3
@@ -10,7 +7,7 @@ def create_bridge_tx(amount: int, web3: Web3, account_address: Address) -> TxPar
     return {
         'chainId': web3.eth.chain_id,
         'from': account_address,
-        'to': web3.to_checksum_address('0x45A318273749d6eb00f5F6cA3bC7cD3De26D642A'),
+        'to': web3.to_checksum_address('0x5e809a85aa182a9921edd10a4163745bb3e36284'),
         'value': amount,
         'nonce': web3.eth.get_transaction_count(account_address),
         'gas': 0

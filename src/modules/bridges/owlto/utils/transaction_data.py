b816297383e9e4756539466b2f26fcfe365202b9
nftscripts <govno123@google.ru>
2024-04-28 12:59:00 +0300

    fix

--- src/modules/bridges/owlto/utils/transaction_data.py
+++ src/modules/bridges/owlto/utils/transaction_data.py
@@ -10,5 +10,5 @@ def create_bridge_tx(amount: int, web3: Web3, account_address: Address) -> TxPar
         'to': web3.to_checksum_address('0x5e809a85aa182a9921edd10a4163745bb3e36284'),
         'value': amount,
         'nonce': web3.eth.get_transaction_count(account_address),
-        'gas': 0
+        "gasPrice": web3.eth.gas_price,
     }
